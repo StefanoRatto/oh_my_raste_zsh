3d40d606d7b7f33ad75b980da17102301ba34480
Stefano Ratto <stefano.ratto@gmail.com>
2022-06-16 23:55:11 -0500

    Commit of resources, sample .zshrc and configuration script.

---
+++ oh_my_raste_zsh.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+cp ~/.zshrc ~/.zshrc.bak
+cp zshrc ~/.zshrc
